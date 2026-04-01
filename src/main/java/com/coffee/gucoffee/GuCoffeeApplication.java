da9fc13ed3185a5fd393a4fb5a5f0d1fd527d27e
gujunhyeong <rnwnsgud90@naver.com>
2026-04-01 20:34:33 +0900

    initial commit

---
+++ src/main/java/com/coffee/gucoffee/GuCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.gucoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffee/gucoffee/GuCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.gucoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
