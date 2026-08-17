df20a84c5b23dc6509f234c37654efc938b8d2f1
gujunhyeong <rnwnsgud90@naver.com>
2026-08-17 17:22:11 +0900

    이벤트 트랜잭션 격리

--- core/core-api/src/main/java/com/coffee/gu/cancel/CancelService.java
+++ core/core-api/src/main/java/com/coffee/gu/cancel/CancelService.java
@@ -1,43 +1,36 @@
 package com.coffee.gu.cancel;
 
+import com.coffee.gu.CancelEvent;
 import com.coffee.gu.PaymentGatewayCancel;
-import com.coffee.gu.Principal;
-import com.coffee.gu.TransactionHistoryManager;
-import com.coffee.gu.coupon.IssuedCouponManager;
 import com.coffee.gu.order.Order;
-import com.coffee.gu.order.OrderManager;
 import com.coffee.gu.payment.Payment;
 import com.coffee.gu.payment.PaymentGatewayProcessor;
 import com.coffee.gu.payment.PaymentReader;
-import com.coffee.gu.enums.TransactionType;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CancelService {
 
-    private final CancelRollbacker cancelRollbacker;
-    private final CancelRecorder cancelRecorder;
     private final CancelValidator validator;
     private final PaymentReader paymentReader;
     private final PaymentGatewayProcessor paymentGatewayProcessor;
+    private final CancelTxHandler cancelTxHandler;
 
-    public CancelService(CancelRollbacker cancelRollbacker, CancelRecorder cancelRecorder, CancelValidator validator, PaymentReader paymentReader, PaymentGatewayProcessor paymentGatewayProcessor) {
-        this.cancelRollbacker = cancelRollbacker;
-        this.cancelRecorder = cancelRecorder;
+    public CancelService(CancelValidator validator, PaymentReader paymentReader, PaymentGatewayProcessor paymentGatewayProcessor, CancelTxHandler cancelTxHandler) {
         this.validator = validator;
         this.paymentReader = paymentReader;
         this.paymentGatewayProcessor = paymentGatewayProcessor;
+        this.cancelTxHandler = cancelTxHandler;
     }
 
-    public Long cancel(Order order, Principal principal) {
+    public void cancel(Order order) {
+        cancel(order, new CancelEvent(order.getKey()));
+    }
+
+    public void cancel(Order order, CancelEvent event) {
         Payment payment = paymentReader.getByOrderKey(order.getKey());
-        // 검증(주문유효성, 주문취소가능여부)
-        validator.validate(order, payment, principal);
-        // pg 취소
+        validator.validate(order, payment);
         paymentGatewayProcessor.cancelPayment(new PaymentGatewayCancel(payment.getExternalPaymentKey(), "구매자 변심"));
-        // 롤백
-        cancelRollbacker.rollback(order, payment);
-        // 기록
-        return cancelRecorder.record(payment, order).getId();
+        cancelTxHandler.completeCancelTx(order, payment, event);
     }
 }
